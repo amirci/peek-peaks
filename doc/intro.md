3db4bc0f6e51295537ec66409f3d749f4c913e07
Amir Barylko <amir@barylko.com>
2017-11-15 10:29:14 -0800

    Empty skeleton

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to peek_peaks
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/peek_peaks/core_test.clj
@@ -0,0 +1,7 @@
+(ns peek-peaks.core-test
+  (:require [clojure.test :refer :all]
+            [peek-peaks.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
